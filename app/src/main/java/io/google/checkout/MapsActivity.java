d27f41734f6c3934f8e815ea3f7045bba5c3f346
CPUPO <cristiano.pupo+github@gmail.com>
2018-03-02 10:02:06 -0300

    STEP 7.4.c original

--- app/src/main/java/io/google/checkout/MapsActivity.java
+++ app/src/main/java/io/google/checkout/MapsActivity.java
@@ -1,18 +1,24 @@
 package io.google.checkout;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
 
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLngBounds.Builder mBounds = new LatLngBounds.Builder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // Pad the map controls to make room for the button - note that the button may not have
+        // been laid out yet.
+        final Button button = (Button) findViewById(R.id.checkout_button);
+        button.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        mMap.setPadding(0, button.getHeight(), 0, 0);
+                    }
+                }
+        );
 
+        //STEP 7.4.c
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                addPointToViewPort(ll);
+                // we only want to grab the location once, to allow the user to pan and zoom freely.
+                mMap.setOnMyLocationChangeListener(null);
+            }
+        });
+
+    }
+
+    //STEP 7.4.b
+    private void addPointToViewPort(LatLng newPoint) {
+        mBounds.include(newPoint);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(mBounds.build(),
+                findViewById(R.id.checkout_button).getHeight()));
     }
 }
